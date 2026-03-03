4757dc2c837ce29acd301b5e3be025f3efb5af41
HafizhArqa <hafizharqamilandri@gmail.com>
2026-03-03 15:23:23 +0700

    Menambahkan TP 02 Pemrograman JavaScript

--- 02_Pemrograman_JavaScript/TP/index.js
+++ 02_Pemrograman_JavaScript/TP/index.js
@@ -1,9 +1,9 @@
-const arr1 = [2,-99999999, 26, 28, -2]; //inifinit mone glitch🤑
+const arr1 = [2, 0, 26, 28, -2]; 
 
 function mulOfArray(arr) {
     let result = 1;
     for (let i = 0; i < arr.length; i = i + 1) {
-       if (arr[i] >= 0) {
+       if (arr[i] > 0) { //modified untuk menghasil kan 1456 dengan tidak menamahkan 0 sebagai bilangan positif
            result = result * arr[i];
        }
     }
