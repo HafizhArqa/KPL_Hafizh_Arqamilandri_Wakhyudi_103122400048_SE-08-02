ce39b9d22411498cf1c80507ceb66f7a4008b23c
HafizhArqa <hafizharqamilandri@gmail.com>
2026-03-17 15:06:35 +0700

    menambahkan TP04

---
+++ 04_GUI_dengan_HTML_dan_CSS/TP/index.js
@@ -0,0 +1,33 @@
+const editorElement = document.getElementById("editor-kecil");
+const hurufKecilElement = document.getElementById("hk");
+editorElement.addEventListener("input", (event) => {
+    const text = event.target.value;
+    
+    let count = 0;
+    for (let char of text) {
+        if (char >= 'a' && char <= 'z') {
+            count++;
+        }
+    }
+
+    hurufKecilElement.textContent = count;
+});
+
+document.getElementById("huruf-besar").addEventListener("click", () => {
+    editorElement.value = editorElement.value.toUpperCase();
+});
+
+document.getElementById("huruf-kecil").addEventListener("click", () => {
+    editorElement.value = editorElement.value.toLowerCase();
+});
+
+const buttonLightElement = document.getElementById("tombol-terang");
+const buttonDarkElement = document.getElementById("tombol-gelap");
+
+buttonLightElement.addEventListener("click", () => {
+    document.documentElement.classList.remove("dark-mode");
+});
+
+buttonDarkElement.addEventListener("click", () => {
+    document.documentElement.classList.add("dark-mode");
+});
