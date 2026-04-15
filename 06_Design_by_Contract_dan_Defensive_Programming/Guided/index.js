64aed3443c1cafaf0f4b629ed2ea568d11ef0ec7
HafizhArqa <hafizharqamilandri@gmail.com>
2026-04-15 07:34:51 +0700

    update TP07

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/Guided/index.js
@@ -0,0 +1,18 @@
+function div(a, b) {
+    if (typeof a !== 'number' && typeof b !== 'number'){
+        throw new TypeError("Salah tipe data untuk argumen a");
+    }
+
+    if (b === 0){
+        return 0;
+    }
+    const hasil = a/b;
+    if (hasil * b === a){
+        return hasil;
+    }
+    return 0;
+}
+
+console.log(
+    div(20, 40)
+);
